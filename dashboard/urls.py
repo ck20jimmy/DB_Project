b65beb747ca132125e7135650540449140238e79
Yu-Hsin Lin <b04705041@ntu.edu.tw>
2018-01-15 01:16:11 +0800

    Set authentication

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,18 +1,21 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required, permission_required
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     url(r'^auth/$', views.LoginView.as_view(), name="login"),
-    url(r'^$', views.IndexView.as_view(), name="index"),
-    url(r'^blank/$', views.BlankView.as_view(), name="blank"),
-    url(r'^buttons/$', views.ButtonsView.as_view(), name="buttons"),
-    url(r'^flot/$', views.FlotView.as_view(), name="flot"),
-    url(r'^forms/$', views.FormsView.as_view(), name="forms"),
-    url(r'^grid/$', views.GridView.as_view(), name="grid"),
-    url(r'^icons/$', views.IconsView.as_view(), name="icons"),
-    url(r'^morris/$', views.MorrisView.as_view(), name="morris"),
-    url(r'^notifications/$', views.NotificationsView.as_view(), name="notifications"),
-    url(r'^panels/$', views.PanelsView.as_view(), name="panels"),
-    url(r'^tables/$', views.TablesView.as_view(), name="tables"),
-    url(r'^typography/$', views.TypographyView.as_view(), name="typography"),
+    url(r'^$', login_required(TemplateView.as_view(template_name="components/index.html"))),
+    url(r'^blank/$', login_required(TemplateView.as_view(template_name="components/blank.html"))),
+    url(r'^buttons/$', login_required(TemplateView.as_view(template_name="components/buttons.html"))),
+    url(r'^flot/$', login_required(TemplateView.as_view(template_name="components/flot.html"))),
+    url(r'^forms/$', login_required(TemplateView.as_view(template_name="components/forms.html"))),
+    url(r'^grid/$', login_required(TemplateView.as_view(template_name="components/grid.html"))),
+    url(r'^icons/$', login_required(TemplateView.as_view(template_name="components/icons.html"))),
+    url(r'^morris/$', login_required(TemplateView.as_view(template_name="components/morris.html"))),
+    url(r'^notifications/$', login_required(TemplateView.as_view(template_name="components/notifications.html"))),
+    url(r'^panels/$', login_required(TemplateView.as_view(template_name="components/panels.html"))),
+    url(r'^tables/$', login_required(TemplateView.as_view(template_name="components/tables.html"))),
+    url(r'^typography/$', login_required(TemplateView.as_view(template_name="components/typography.html"))),
+    url(r'^logout/$', views.LogoutView.as_view(), name="logout"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,64 +7,52 @@ from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
 from django.views.generic import FormView, RedirectView
+from django.contrib.auth.decorators import login_required
 
 class IndexView(TemplateView):
     template_name = "components/index.html"
-
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context.update({'title': "Dashboard"})
         return context
 
-
 class BlankView(TemplateView):
     template_name = "components/blank.html"
-
     def get_context_data(self, **kwargs):
         context = super(BlankView, self).get_context_data(**kwargs)
         context.update({'title': "Blank Page"})
         return context
 
-
 class ButtonsView(TemplateView):
     template_name = "components/buttons.html"
-
     def get_context_data(self, **kwargs):
         context = super(ButtonsView, self).get_context_data(**kwargs)
         context.update({'title': "Buttons"})
         return context
 
-
 class FlotView(TemplateView):
     template_name = "components/flot.html"
-
     def get_context_data(self, **kwargs):
         context = super(FlotView, self).get_context_data(**kwargs)
         context.update({'title': "Flot Charts"})
         return context
 
-
 class FormsView(TemplateView):
     template_name = "components/forms.html"
-
     def get_context_data(self, **kwargs):
         context = super(FormsView, self).get_context_data(**kwargs)
         context.update({'title': "Forms"})
         return context
 
-
 class GridView(TemplateView):
     template_name = "components/grid.html"
-
     def get_context_data(self, **kwargs):
         context = super(GridView, self).get_context_data(**kwargs)
         context.update({'title': "Grid"})
         return context
 
-
 class IconsView(TemplateView):
     template_name = "components/icons.html"
-
     def get_context_data(self, **kwargs):
         context = super(IconsView, self).get_context_data(**kwargs)
         context.update({'title': "Icons"})
@@ -105,47 +93,43 @@ class LoginView(FormView):
 
                                                                     
 
-
 class MorrisView(TemplateView):
     template_name = "components/morris.html"
-
     def get_context_data(self, **kwargs):
         context = super(MorrisView, self).get_context_data(**kwargs)
         context.update({'title': "Morris Charts"})
         return context
 
-
 class NotificationsView(TemplateView):
     template_name = "components/notifications.html"
-
     def get_context_data(self, **kwargs):
         context = super(NotificationsView, self).get_context_data(**kwargs)
         context.update({'title': "Notifications"})
         return context
 
-
 class PanelsView(TemplateView):
     template_name = "components/panels-wells.html"
-
     def get_context_data(self, **kwargs):
         context = super(PanelsView, self).get_context_data(**kwargs)
         context.update({'title': "Panels and Wells"})
         return context
 
-
 class TablesView(TemplateView):
     template_name = "components/tables.html"
-
     def get_context_data(self, **kwargs):
         context = super(TablesView, self).get_context_data(**kwargs)
         context.update({'title': "Tables"})
         return context
 
-
 class TypographyView(TemplateView):
     template_name = "components/typography.html"
-
     def get_context_data(self, **kwargs):
         context = super(TypographyView, self).get_context_data(**kwargs)
         context.update({'title': "Typography"})
         return context
+
+class LogoutView(RedirectView):
+    url = '/auth/'
+    def get(self, request, *args, **kwargs):
+        auth_logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
