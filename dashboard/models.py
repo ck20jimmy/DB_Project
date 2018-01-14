3a613739907caa5355a5cd63778a48a40564539f
ck20jimmy <b04705018@ntu.im>
2018-01-15 05:05:49 +0800

    add SwitchView

---
+++ dashboard/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+class Personnel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
+    email = models.EmailField()
+    name = models.CharField(max_length=50, null=True)
+    def __str__(self):
+        return '%s (%s)' % (self.name, self.email)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Personnel.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.personnel.email = instance.email
+    instance.personnel.save()
+
+class Server_account(models.Model):
+    personnel_id = models.ForeignKey('Personnel', on_delete=models.SET_NULL, null=True)  
+    username = models.CharField(max_length=50, null=True)
+    privilege = models.CharField(max_length=50, null=True)
+    server_id = models.ForeignKey('Server', on_delete=models.CASCADE)
+    password = models.CharField(max_length=50, null=True)
+    def __str__(self):
+        return self.username + '@' + self.server_id
+
+class Switch_account(models.Model):
+    personnel_id = models.ForeignKey('Personnel',on_delete=models.SET_NULL, null=True)  
+    username = models.CharField(max_length=50, null=True)
+    privilege = models.CharField(max_length=50, null=True)
+    switch_id = models.ForeignKey('Switch', on_delete=models.CASCADE)
+    password = models.CharField(max_length=50, null=True)
+    def __str__(self):
+        return str(self.username) + '@' + str(self.switch_id)
+
+class Server(models.Model):
+    slot_num = models.IntegerField(null=True)
+    cabinet_num = models.IntegerField(null=True)
+    spec = models.CharField(max_length=300, null=True)
+    brand_and_model = models.CharField(max_length=100, null=True)
+
+class Service(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    server_id = models.ForeignKey('Server', on_delete=models.CASCADE)
+    port_id = models.IntegerField(null=True)
+    interface_id = models.ForeignKey('Interface', on_delete=models.CASCADE)
+
+class Switch(models.Model):
+    ip = models.CharField(max_length=20, null=True)
+    slot_num = models.IntegerField(null=True)  
+    brand_and_model = models.CharField(max_length=100, null=True)
+    cabinet_num = models.IntegerField(null=True)
+
+class Interface(models.Model):
+    ip = models.CharField(max_length=20, null=True)
+    hostname = models.CharField(max_length=100, null=True)
+    server_id = models.ForeignKey('Server', on_delete=models.CASCADE)
+    port_id = models.ForeignKey('Port', on_delete=models.CASCADE)
+
+class Port(models.Model):
+    switch_port_id = models.IntegerField(null=True)
+    switch_id = models.ForeignKey('Switch', on_delete=models.CASCADE)
+
+class Access_to_server(models.Model):
+    class Meta:
+        unique_together = (('username', 'server_id'),)
+    username = models.ForeignKey('Server_account', on_delete=models.CASCADE)
+    server_id = models.ForeignKey('Server', on_delete=models.CASCADE)
+
+class Access_to_service(models.Model):
+    class Meta:
+        unique_together = (('username', 'service_id'),)
+    username = models.ForeignKey('Server_account', on_delete=models.CASCADE)
+    service_id = models.ForeignKey('Service', on_delete=models.CASCADE)
+
+class Access_to_switch(models.Model):
+    class Meta:
+        unique_together = (('username', 'switch_id'),)
+    username = models.ForeignKey('Switch_account', on_delete=models.CASCADE)
+    switch_id = models.ForeignKey('Switch', on_delete=models.CASCADE)
+
+class Vlan_to_interface(models.Model):
+    class Meta:
+        unique_together = (('vlan_id', 'interface_id'),)
+    vlan_id = models.IntegerField(null=True)
+    interface_id = models.ForeignKey('Interface', on_delete=models.CASCADE)
